5d2b26b0e98f2eec844cfe6e20f993de4e58586c
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-07-15 01:29:54 +0900

    miss

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,4 @@
-import { useSession, getSession, GetSessionParams } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Profile() {
   const { data: session } = useSession();
@@ -16,23 +16,3 @@ export default function Profile() {
     </div>
   );
 }
-
-// 서버사이드 렌더링을 위해 세션 정보를 프리페치합니다.
-// export async function getServerSideProps(
-//   context: GetSessionParams | undefined
-// ) {
-//   const session = await getSession(context);
-
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: "api/auth/signin",
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     props: { session },
-//   };
-// }
