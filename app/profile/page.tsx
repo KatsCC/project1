0239c0631172919889acc1648945fcd9d13181b9
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-07-15 01:42:04 +0900

    miss

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,16 +1,4 @@
-import { useSession } from "next-auth/react";
-
 export default function Profile() {
-  const { data: session, status } = useSession();
-
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
-
-  if (!session) {
-    return <p>You are not logged in.</p>;
-  }
-
   return (
     <div>
       <h1>Welcome</h1>
