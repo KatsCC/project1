682c36128e27f98be817fe6a96b04bd78490a751
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-07-15 01:14:51 +0900

    miss

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,13 +3,15 @@ import { useSession, getSession, GetSessionParams } from "next-auth/react";
 export default function Profile() {
   const { data: session } = useSession();
 
+  console.log(session);
+
   if (!session) {
     return <p>Loading...</p>;
   }
 
   return (
     <div>
-      <h1>Welcome, {session.user.name}</h1>
+      <h1>Welcome,</h1>
       <p>Email: </p>
     </div>
   );
