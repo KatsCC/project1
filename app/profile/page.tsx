f75e09ff2ebc119d8a2a8c805548037ebc0d1a0c
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-07-15 01:36:18 +0900

    miss

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,33 @@
-import { useSession } from "next-auth/react";
+import { getSession, GetSessionParams } from "next-auth/react";
 
-export default function Profile() {
-  const { data: session } = useSession();
-
-  console.log(session);
-
+export default function Profile({ session }: any) {
   if (!session) {
     return <p>Loading...</p>;
   }
 
   return (
     <div>
-      <h1>Welcome,</h1>
-      <p>Email: </p>
+      <h1>Welcome</h1>
+      <p>Email: {session.userData.name}</p>
     </div>
   );
+}
+
+export async function getServerSideProps(
+  context: GetSessionParams | undefined
+) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
