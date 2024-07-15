22898c30b34e2aaa713f44939be7368b9a354843
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-07-15 13:26:47 +0900

    test

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,8 +1,24 @@
 import { db } from "@vercel/postgres";
+import { useEffect, useState } from "react";
 
 export default function Signin() {
-  const client = db.connect();
-  console.log(client);
+  const [client, setClient] = useState({}); // 클라이언트 상태 추가
+
+  useEffect(() => {
+    const connectToDatabase = async () => {
+      try {
+        const dbClient = await db.connect(); // 비동기 연결 함수 호출
+        setClient(dbClient); // 클라이언트 상태 설정
+        console.log(dbClient); // 연결된 클라이언트 출력
+      } catch (error) {
+        console.error("Failed to connect to database:", error);
+        // 에러 처리 로직 추가
+      }
+    };
+
+    connectToDatabase(); // useEffect 내에서 비동기 함수 호출
+  }, []);
+
   return (
     <div>
       <h1>Welcome</h1>
