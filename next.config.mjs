3a806c512e3fa08ca1fc465cfecc71097174a147
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-07-15 16:21:29 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/app/api/auth/:path*", // 요청 경로 설정
-        destination: "/api/auth/[...nextauth]/route.ts", // 실제 파일 경로
-      },
-    ];
-  },
-};
+export async function rewrites() {
+  return [
+    {
+      source: "/app/api/auth/:path*",
+      destination: "/api/auth/[...nextauth]/route.ts",
+    },
+  ];
+}
 
 export default nextConfig;
