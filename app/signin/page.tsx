9bd2f7ba0512487a9d2682c998c0a03415ced0de
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-07-15 13:08:03 +0900

    test

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,12 @@
+import { db } from "@vercel/postgres";
+
+const client = await db.connect();
+const result = await client.query("SELECT * FROM userdata WHERE userId = cat");
+
 export default function Signin() {
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome {result.rows}</h1>
       <p>Email</p>
     </div>
   );
